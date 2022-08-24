0f15cf213a0092dd5128ad624b6483e5a5625a78
Leejaehan <jahan4707@naver.com>
2022-08-24 19:24:14 +0900

    modify 16234 인구이동

--- BOJ/Implement/16234_인구이동/LJH/16234.cpp
+++ BOJ/Implement/16234_인구이동/LJH/16234.cpp
@@ -14,7 +14,7 @@ vector<int> x = { 1, -1, 0, 0 }, y = { 0, 0, 1, -1 };
 queue<pair<int, int>> q;
 vector<vector<int>> visit;
 bool flag;
-void bfs();
+void bfs(int r, int c);
 void path(int r, int c);
 map<pair<int, int>, int> m;
 int main()
@@ -28,14 +28,25 @@ int main()
         for (int j = 0; j < n; j++)
         {
             cin >> ary[i][j];
-            q.push(make_pair(i, j));
         }
     }
     while (1)
     {
-        bfs();
-        if (flag == true)
+        flag = false;
+        for (int i = 0; i < n; i++)
         {
+            for (int j = 0; j < n; j++)
+            {
+                if (visit[i][j] == 0)
+                {
+                    m.insert(make_pair(make_pair(i, j), 0));
+                    bfs(i, j);
+                    sum = 0, cnt = 0;
+                }
+            }
+        }
+        visit.assign(n, vector<int>(n, 0));
+        if (flag == true) {
             d++;
         }
         else
@@ -43,19 +54,16 @@ int main()
     }
     cout << d << endl;
 }
-void bfs()
+void bfs(int i, int j)
 {
-    int Size = q.size();
-    flag = false;
-    cnt = 0;
-    sum = 0;
-    while (Size != 0)
+    q.push(make_pair(i, j));
+    visit[i][j] = 1;
+    while (!q.empty())
     {
         auto a = q.front(); // a.firts에는 row, a.second에는 col이 저장됨.
-
         q.pop();
-        q.push(make_pair(a.first, a.second));
-        Size--;
+        sum = sum + ary[a.first][a.second];
+        cnt = cnt + 1;
         for (int i = 0; i < 4; i++)
         {
             int mx = a.first + x[i], my = a.second + y[i];
@@ -63,65 +71,27 @@ void bfs()
             {
                 continue;
             }
+            if (visit[mx][my] != 0)
+            {
+                continue;
+            }
             if (abs(ary[a.first][a.second] - ary[mx][my]) >= L && abs(ary[a.first][a.second] - ary[mx][my]) <= R)
             {
                 //인구수가 l~r 사이라면 국경을 열어야함.
+                q.push(make_pair(mx, my));
+                m.insert(make_pair(make_pair(mx, my), 0));
+                visit[mx][my] = 1;
                 flag = true;
-                if (visit[a.first][a.second] == 0)
-                {
-                    visit[a.first][a.second] = 1; //국경선을 하나라도 열었으면
-                }
             }
         }
-        //한 나라에 대한 국경선 열기 끝.
     }
     if (flag == true)
     {
-        for (int i = 0; i < n; i++)
+        int people = sum / cnt;
+        for (auto iter = m.begin(); iter != m.end(); iter++)
         {
-            for (int j = 0; j < n; j++)
-            {
-                if (visit[i][j] == 1)
-                {
-                    m.insert(make_pair(make_pair(i, j), 0));
-                    sum += ary[i][j];
-                    path(i, j);
-                    int SSS = m.size();
-                    int people = sum / SSS;
-                    for (auto iter = m.begin(); iter != m.end(); iter++)
-                    {
-                        ary[iter->first.first][iter->first.second] = people;
-                    }
-                    sum = 0;
-                    m.clear();
-                }
-            }
+            ary[iter->first.first][iter->first.second] = people;
         }
     }
-    visit.assign(n, vector<int>(n, 0));
-}
-void path(int r, int c)
-{
-    if (visit[r][c] == -1)
-    {
-        return;
-    }
-    visit[r][c] = -1;
-    for (int i = 0; i < 4; i++)
-    {
-        int dx = r + x[i], dy = c + y[i];
-        if (dx < 0 || dx >= n || dy < 0 || dy >= n)
-        {
-            continue;
-        }
-        if (abs(ary[r][c] - ary[dx][dy]) < L || abs(ary[r][c] - ary[dx][dy]) > R) {
-            continue;
-        }
-        if (visit[dx][dy] == 1)
-        {
-            m.insert(make_pair(make_pair(dx, dy), 0));
-            sum += ary[dx][dy];
-            path(dx, dy);
-        }
-    }
+    m.clear();
 }
