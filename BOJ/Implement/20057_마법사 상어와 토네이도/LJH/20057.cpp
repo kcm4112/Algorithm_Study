8bef199f398664cb4401938102d8e7e64b8bd04d
이재한 <99114456+jaehan4707@users.noreply.github.com>
2022-09-15 21:30:56 +0900

    modify 20057 마법사 상어와 토네이도

--- BOJ/Implement/20057_마법사 상어와 토네이도/LJH/20057.cpp
+++ BOJ/Implement/20057_마법사 상어와 토네이도/LJH/20057.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 int n;
 vector<vector<int>> g;
-vector<int> x{ 0, 1, 0, -1 };
-vector<int> y{ -1, 0, 1, 0 };
-vector<double> z{ 0.01, 0.07, 0.1, 0.02, 0.05 };
+vector<int> x{0, 1, 0, -1};
+vector<int> y{-1, 0, 1, 0};
+vector<double> z{0.01, 0.07, 0.1, 0.02, 0.05};
 int out = 0, step = 0, dist = 1;
 void location(int a, int b, int temp, int k);
 int main()
@@ -25,15 +25,14 @@ int main()
     }
     int nowR = n / 2, nowC = n / 2, afterR, afterC, move = 0;
     bool check = true;
-    //2번 회전할때마다 거리가 1씩 증가.
     while (check)
-    { // 1,1까지 이동한 뒤 소멸해야함.
+    {                                 // 1,1까지 이동한 뒤 소멸해야함.
         int csand = g[nowR][nowC];    //현재 좌표에서 모래
-        afterR = nowR + x[move];      //움직여 준다.
-        afterC = nowC + y[move];      //움직여 준다.
-        int temp = g[afterR][afterC]; //움직인 자리에 있는 모래.
-        g[afterR][afterC] = csand;
-        int sum = 0;
+        afterR = nowR + x[move];      //이동한 x좌표.
+        afterC = nowC + y[move];      //이동한 y좌표
+        int temp = g[afterR][afterC]; //이동한 자리에 있던 모래
+        g[afterR][afterC] = csand; //이동한 자리에 있던 모래를 기존의 모래로 채운다.
+        int sum = 0; //모래의 양
         if (move == 0)
         { //왼쪽으로 1칸
             step += 1;
@@ -46,9 +45,9 @@ int main()
             }
             location(afterR + 2, afterC, temp, 3);
             location(afterR - 2, afterC, temp, 3);
+            location(nowR + 3 * x[move], nowC + 3 * y[move], temp, 4); // a + 진행방향
             sum += temp * z[3];
             sum += temp * z[3];
-            location(nowR + 3 * x[move], nowC + 3 * y[move], temp, 4); //a + 진행방향
             sum += temp * z[4];
             // sum에는 이제 다 분배된 모래가 들어있음. 이제 나머지는 알파에 넣어야함.
             if (afterR + x[move] < 0 || afterR + x[move] >= n || afterC + y[move] < 0 || afterC + y[move] >= n)
@@ -59,7 +58,7 @@ int main()
                 g[afterR + x[move]][afterC + y[move]] += temp - sum;
         }
         else if (move == 1)
-        { //아래로 한칸                    
+        { //아래로 한칸
             step += 1;
             for (int k = 0; k < 3; k++)
             {
@@ -71,9 +70,9 @@ int main()
 
             location(afterR, afterC + 2, temp, 3);
             location(afterR, afterC - 2, temp, 3);
-            sum += temp * z[3];
-            sum += temp * z[3];
             location(nowR + 3 * x[move], nowC + 3 * y[move], temp, 4);
+            sum += temp * z[3];
+            sum += temp * z[3];
             sum += temp * z[4];
             // sum에는 이제 다 분배된 모래가 들어있음. 이제 나머지는 알파에 넣어야함.
             if (afterR + x[move] < 0 || afterR + x[move] >= n || afterC + y[move] < 0 || afterC + y[move] >= n)
@@ -84,7 +83,7 @@ int main()
                 g[afterR + x[move]][afterC + y[move]] += temp - sum;
         }
         else if (move == 2)
-        { // right           
+        { // right
             step += 1;
             for (int k = 0; k < 3; k++)
             {
@@ -93,11 +92,11 @@ int main()
                 sum += temp * z[k];
                 sum += temp * z[k];
             }
-            sum += temp * z[3];
             location(afterR + 2, afterC, temp, 3);
             location(afterR - 2, afterC, temp, 3);
-            sum += temp * z[3];
             location(nowR + 3 * x[move], nowC + 3 * y[move], temp, 4);
+            sum += temp * z[3];
+            sum += temp * z[3];
             sum += temp * z[4];
             // sum에는 이제 다 분배된 모래가 들어있음. 이제 나머지는 알파에 넣어야함.
             if (afterR + x[move] < 0 || afterR + x[move] >= n || afterC + y[move] < 0 || afterC + y[move] >= n)
@@ -108,7 +107,7 @@ int main()
                 g[afterR + x[move]][afterC + y[move]] += temp - sum;
         }
         else if (move == 3)
-        { // up            
+        { // up
             step += 1;
             for (int k = 0; k < 3; k++)
             {
@@ -118,11 +117,10 @@ int main()
                 sum += temp * z[k];
             }
             location(afterR, afterC + 2, temp, 3);
-            sum += temp * z[3];
             location(afterR, afterC - 2, temp, 3);
-            sum += temp * z[3];
-            //  g[nowR + 3 * x[move]][nowC + 3 * y[move]] += temp * z[4];
             location(nowR + 3 * x[move], nowC + 3 * y[move], temp, 4);
+            sum += temp * z[3];
+            sum += temp * z[3];
             sum += temp * z[4];
             // sum에는 이제 다 분배된 모래가 들어있음. 이제 나머지는 알파에 넣어야함.
             if (afterR + x[move] < 0 || afterR + x[move] >= n || afterC + y[move] < 0 || afterC + y[move] >= n)
@@ -132,23 +130,26 @@ int main()
             else
                 g[afterR + x[move]][afterC + y[move]] += temp - sum;
         }
+        
         //다 이동하면 이제 현재 좌표를 옮겨준다.
         nowR = afterR, nowC = afterC;
-        if (nowR == 0 && nowC == 0) {
-            check = false;
+        if (nowR == 0 && nowC == 0) //만약 탈출 지점까지 왔다면 while문을 탈출해야함.
+        {
+            break;
         }
-        if (move % 2 == 1 && step == dist) {
-            dist = dist + 1; //dist만큼 방향을 전진 시켜야함.
+        if (move % 2 == 1 && step == dist) //방향이 2번 바뀔때마다 거리를 1씩 증가시켜줘야함.
+        {
+            dist = dist + 1; // dist만큼 방향을 전진 시켜야함.
             move = (move + 1) % 4;
             step = 0;
         }
-        if (step == dist) {
+        if (step == dist)
+        {
             move = (move + 1) % 4;
             step = 0;
         }
-        //방향이 2번 바뀔때마다 거리를 1씩 증가시켜줘야함.
     }
-    cout << out;
+    cout << out; //빠져나간 모래의 양
 }
 void location(int a, int b, int temp, int k)
 {
@@ -156,6 +157,4 @@ void location(int a, int b, int temp, int k)
         out += temp * z[k];
     else
         g[a][b] += temp * z[k];
-
 }
-
